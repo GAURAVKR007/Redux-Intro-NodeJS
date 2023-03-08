f27c6070eb202f9583d64c2e22f9d7103015cc54
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-03-08 14:58:51 +0530

    Added middleware -> logger and log every action

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const redux = require('redux')
 const createStore = redux.createStore
 const bindActionCreators = redux.bindActionCreators
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_STOCK = 'CAKE_STOCK'
@@ -100,9 +104,9 @@ const rootReducer = combineReducers({
     iceCream: IcecreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 console.log('Initial state', store.getState())
-const unsubscribe = store.subscribe(()=> console.log('Update state', store.getState()))
+const unsubscribe = store.subscribe(()=> {})
 
 // store.dispatch(orderCake())
 // store.dispatch(orderCake())
